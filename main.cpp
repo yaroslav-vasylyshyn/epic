5a7536125a55116456f0bacc4374fd4d1518edd4
yaroslav-vasylyshyn <146115906+yaroslav-vasylyshyn@users.noreply.github.com>
2023-09-30 11:59:17 +0300

    1.0.1
    adding min

--- main.cpp
+++ main.cpp
@@ -3,15 +3,21 @@ using namespace std;
 
 int main(){
     int m,max;
+    long long min;
     cin>>m;
     max=0;
+    min=20000000000;
     int arr[m];
     for(int i=0;i<m;i++){
         cin>>arr[i];
         if (max<arr[i]){
             max=arr[i];
         }
+        if (min>arr[i]){
+            min=arr[i];
+        }
     } 
-    cout<<max;
+    cout<<max<<" "<<min;
+
     return 0;
 }
